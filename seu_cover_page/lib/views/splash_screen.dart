7b29abfaa7415e425d53af2f42e1b11f37ce1a7a
sadman_ichha <sadmanichha1@gmail.com>
2024-03-16 21:04:22 +0600

    added: Splash Screen

--- seu_cover_page/lib/views/splash_screen.dart
+++ seu_cover_page/lib/views/splash_screen.dart
@@ -5,6 +5,10 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        
+      ),
+    );
   }
 }
